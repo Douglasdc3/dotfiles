ba7e3b6933c2762a14f35e8f8c17126c137a640b
Ken Andries <ken.andries.1992@gmail.com>
2016-09-27 20:30:16 +0200

    [ADD] helper to commit tag and push new tag

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,2 +1,17 @@
 alias gcd=git checkout development
 alias gcm=git checkout master
+
+
+composer-bump()
+{
+    VERSION=$(cat composer.json | grep version | head -1 | awk -F: '{ print $2}' | sed 's/[",]//g' | tr -d '[[:space:]]')
+    MESSAGE="[BUMP] $VERSION"
+    VERSION_STRING="v$VERSION"
+
+    echo $VERSION_STRING
+    git commit -m "$MESSAGE"
+    git tag $VERSION_STRING
+
+    git push
+    git push --tag
+}
