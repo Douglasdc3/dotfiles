a19c1dffc771aecc0713bdba3f144c0cab0ddec2
Ken Andries <ken.andries.1992@gmail.com>
2022-12-20 20:07:39 +0100

    Add nvim keymappings

---
+++ nvim/lua/user/keymaps.lua
@@ -0,0 +1,50 @@
+-- Space is my leader.
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- When text is wrapped, move by terminal rows, not lines, unless a count is provided
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+
+-- Reselect visual selection after indenting
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
+
+-- Maintain the cursor position when yanking a visual selection
+-- http://ddrscott.github.io/blog/2016/yank-without-jank/
+vim.keymap.set('v', 'y', 'myy`y')
+vim.keymap.set('v', 'Y', 'myY`y')
+
+-- Disable annoying command line thing
+vim.keymap.set('n', 'q:', ':q<CR>')
+
+-- Paste replace visual selection without copying it
+vim.keymap.set('v', 'p', '"_dP')
+
+-- Easy insertion of a trailing ; or , from insert mode
+vim.keymap.set('i', ';;', '<Esc>A;<Esc>')
+vim.keymap.set('i', ',,', '<Esc>A,<Esc>')
+
+-- Quickly clear search highlight
+vim.keymap.set('n', '<leader>k', ':nohlsearch<cr>')
+
+-- Open the current file in the default program (on Mac this should just be just `open`)
+vim.keymap.set('n', '<leader>x', ':!open %<cr><cr>')
+
+vim.keymap.set('n', '<leader>q', ':bufdo bdelete<cr>')
+
+-- Allow gf to open non-existent files
+vim.keymap.set('', 'gf', ':edit <cfile><CR>')
+
+-- Move text up and down
+vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
+vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi')
+vim.keymap.set('x', '<A-j>', ":move '>+1<CR>gv-gv")
+vim.keymap.set('x', '<A-k>', ":move '<-2<CR>gv-gv")
+
+-- Resize with arrows
+vim.keymap.set('n', '<C-Up>', ':resize +2<CR>')
+vim.keymap.set('n', '<C-Down>', ':resize -2<CR>')
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
+
