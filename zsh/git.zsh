dc03061c8cef14bf1bd0a86fcd8859f18c7e69cb
Ken Andries <ken.andries.1992@gmail.com>
2023-06-14 00:39:22 +0200

    Update dotfiles

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -1,2 +1,5 @@
 alias gpb='git push --set-upstream origin "$(g rev-parse --abbrev-ref HEAD)"' # Push branch to upstream
 alias gcl='g branch | grep -v "master" | xargs g branch -d' # Clean deleted branches
+alias co='git checkout'
+alias develop='git checkout develop'
+alias main='git checkout $([ `git rev-parse --quiet --verify master` ] && echo "master" || echo "main")' # Change to main branch
