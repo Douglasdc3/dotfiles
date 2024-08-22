3bb04910b3343acc02e240f54aa9968e0214a2db
Ken Andries <ken.andries.1992@gmail.com>
2024-08-22 11:21:37 +0200

    Fix docker compose syntax on new docker versions

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -2,7 +2,7 @@ alias d="docker"
 alias dc="docker compose"
 alias dce="docker compose exec"
 dcea () {
-    docker compose exec $(docker-compose ps --services 2>/dev/null | grep -v '^event\|web-.*' | head -n1) $*
+    docker compose exec $(docker compose ps --services 2>/dev/null | grep -v '^event\|web-.*' | head -n1) $*
 }
 dceas () {
     docker compose exec app su app -c "$*"
