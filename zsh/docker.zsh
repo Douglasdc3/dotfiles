4f1ddde561a0f6cda8501b8af9f8e8b4f842aa05
Ken Andries <ken.andries.1992@gmail.com>
2024-08-22 11:00:52 +0200

    Update docker compose alias

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,11 +1,11 @@
 alias d="docker"
-alias dc="docker-compose"
-alias dce="docker-compose exec"
+alias dc="docker compose"
+alias dce="docker compose exec"
 dcea () {
-    docker-compose exec $(docker-compose ps --services 2>/dev/null | grep -v '^event\|web-.*' | head -n1) $*
+    docker compose exec $(docker-compose ps --services 2>/dev/null | grep -v '^event\|web-.*' | head -n1) $*
 }
 dceas () {
-    docker-compose exec app su app -c "$*"
+    docker compose exec app su app -c "$*"
 }
 alias dceasa="dceas php artisan"
 
