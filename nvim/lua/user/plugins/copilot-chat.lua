832ffad8806ca8e6edc345faa02774a4123ad805
Ken Andries <ken.andries.1992@gmail.com>
2024-12-16 20:00:33 +0100

    Update CopilotChat

--- nvim/lua/user/plugins/copilot-chat.lua
+++ nvim/lua/user/plugins/copilot-chat.lua
@@ -4,20 +4,11 @@ return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     dependencies = {
-      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+      { "github/copilot.vim" },            -- or github/copilot.vim
+      { "nvim-lua/plenary.nvim" },         -- for curl, log wrapper
+      { "nvim-telescope/telescope.nvim" }, -- Use telescope for help actions
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
-      -- See Configuration section for options
-      -- window = {
-      --   layout = 'float',
-      --   relative = 'cursor',
-      --   width = 1,
-      --   height = 0.4,
-      --   row = 1
-      -- },
     },
-    -- See Commands section for default commands if you want to lazy load on them
   },
 }
